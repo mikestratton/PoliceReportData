22bd19e0367a5057eaeec2f248b422eda78ba2e7
mikestratton <mike@mikestratton.net>
2017-12-28 03:32:28 -0500

    can now parse pdf document into plain text

---
+++ ohio/county/franklin/city/columbus/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 12/28/2017
+ * Time: 12:09 AM
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>State:Ohio, County:Franklin, City:Columbus</title>
+</head>
+<body>
+    <p>Requirements
+    </p>
+    <code>
+        ONE: <br>
+        Pull data from: http://columbuspolice.org/bulkreports/
+        <br><br>
+        TWO: <br>
+        if(year => 2010 && crash_severity => 2 && crash_severity < 5){ <br>
+            echo: <br>
+                Name, number (all cars involved) -> which party was at fault? <br>
+                Year, make & Model of vehicles <br>
+                insurance company name <br>
+                if(vehicle towed) {display location towed to} <br>
+                crash_severity(2-4) <br>
+                crash report number <br>
+                date of the crash <br>
+        }
+    </code>
+</body>
+</html>
